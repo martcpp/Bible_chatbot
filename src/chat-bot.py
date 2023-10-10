dc87838bc40cd99f003410af1fda9065d0706e72
martcpp <mordecaiemmanueletukudo@gmail.com.com>
2023-10-10 08:33:15 +0100

    add the right function

--- src/chat-bot.py
+++ src/chat-bot.py
@@ -9,19 +9,21 @@ openai.api_key = os.getenv("open.API_KEY")
 
 # Function to generate a response
 def generate_response(user_input):
+    prompt = user_input + "\nbible chatbot:\n"
+
     try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
                 {
                     "role": "user",
-                    "content": user_input
+                    "content": prompt
                 }
             ],
             temperature=0.15,
             max_tokens=1500  # Adjust the token limit as needed
         )
-
+        
         return response.choices[0].message["content"].strip()
 
     except Exception as e:
@@ -29,12 +31,7 @@ def generate_response(user_input):
 
 # Main loop to interact with the user
 if __name__ == "__main__":
-    while True:
-        user_input = input("You: ")
-        prompt = user_input + "\nbible chatbot:\n"
-        # Exit the loop if the user enters "exit" or "quit"
-        if user_input.lower() in ["exit", "quit"]:
-            break
-        
-        response = generate_response(user_input)
-        print("bible bot:", response)
+    print("Welcome to the Bible Chatbot. Type \"exit\" or \"quit\" to end the conversation.")
+    prompt = input("You: ")
+    response = generate_response(prompt)
+    print(response)
