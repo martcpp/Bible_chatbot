a35c07c27f0d8e38336018e3f2c31497f59b7320
martcpp <mordecaiemmanueletukudo@gmail.com.com>
2023-10-12 01:41:09 +0100

    addd ed api and other stuff need

---
+++ src/chat-api.py
@@ -0,0 +1,24 @@
+from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask, request
+from chatbot import generate_response
+
+
+app = Flask(__name__)
+
+@app.route("/bot", methods=["POST"])
+def bot():
+    incoming_msg = request.values.get("Body", "").lower()
+    print(incoming_msg)
+    
+    
+    # msg = resp.message()
+    response = generate_response(incoming_msg)
+    print(response)
+    resp = MessagingResponse()
+    msg = resp.message()
+    msg.body(response)
+    
+    return str(resp)
+    
+if  __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=False, port=5000)
