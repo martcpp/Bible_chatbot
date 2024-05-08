07d0fd7fc072bbd7f63483314092b5745f78c011
martcpp <mordecaiemmanueletukudo@gmail.com>
2024-05-08 14:49:07 +0100

    added file

--- src/bibleAI.py
+++ src/bibleAI.py
@@ -5,8 +5,7 @@ ai = MetaAI()
 
 def bible_verse(verse):
     explanation_text = f" in a layman understanding explain \"{verse}\" not more than 250 characters"
-    prayer_text = f"give a prayer point that fit the this bible verse {
-        verse}not more than 250 characters"
+    prayer_text = f"give a prayer point that fit the this bible verse {verse} not more than 250 characters"
     explanation = ai.prompt(message=explanation_text)
     prayer_point = ai.prompt(message=prayer_text)
     if explanation is not None and prayer_point is not None:
